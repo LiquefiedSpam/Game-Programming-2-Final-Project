d85cbb539ad4878b6124b271301703659af9796f
marcellalmiller <149106160+marcellalmiller@users.noreply.github.com>
2026-03-28 02:40:36 -0400

    Audio

--- Assets/Scripts/Interactable/MerchantBehavior.cs
+++ Assets/Scripts/Interactable/MerchantBehavior.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 public class MerchantBehavior : InteractableBehavior
 {
     [SerializeField] MerchantStallUI stallUI;
+    [SerializeField] AudioSource audioSource;
 
     public override void Interact()
     {
+        audioSource.Play();
         stallUI.Show();
         base.Interact();
     }
 
     public override void Quit()
     {
+        audioSource.Stop();
         stallUI.Hide();
         base.Quit();
     }
